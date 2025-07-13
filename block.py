7e7a1428002292d6d593c7ee9863134dca34963c
Hesam Ro <hesamroustaei80@gmail.com>
2025-07-13 23:17:24 +0330

    Add blocks and detect collision with the ball

---
+++ block.py
@@ -0,0 +1,10 @@
+from turtle import Turtle
+
+class Block(Turtle):
+    def __init__(self, position, color):
+        super().__init__()
+        self.shape("square")
+        self.color(color)
+        self.penup()
+        self.shapesize(stretch_wid=1.35, stretch_len=3.2)  # Makes it rectangular
+        self.goto(position)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from ball import Ball
 from paddle import Paddle
+from block import Block
 import time
 
 
@@ -17,10 +18,30 @@ screen.listen()
 screen.onkey(paddle.go_right, "Right")
 screen.onkey(paddle.go_left, "Left")
 
+# Creating blocks
+blocks = []
+start_x = -240
+start_y = 260
+rows = 4
+columns = 7
+x_spacing = 80
+y_spacing = 40
+colors = ["#3e51b5","#b53e3e","#b5b13e","#54a644"]
+for row in range(rows):
+    for col in range(columns):
+        x = start_x + col * x_spacing
+        y = start_y - row * y_spacing
+        color = colors[row]
+        block = Block(position=(x, y), color=color)
+        blocks.append(block)
+
 game_is_on = True
+counter = 0
+time_sleep = 0.008
+
 while game_is_on:
     screen.update()
-    time.sleep(0.009)
+    time.sleep(time_sleep)
     ball.move()
 
     # Detect collision with upper wall
@@ -33,5 +54,17 @@ while game_is_on:
     if ball.distance(paddle) < 50 and ball.ycor() < - 230:
         ball.bounce_y()
 
+    # Detect collision with blocks
+    for block in blocks[:]:
+        if ball.distance(block) < 50:
+            blocks.remove(block)
+            block.hideturtle()
+            block.goto(1000, 1000)
+            counter += 1
+            if counter % 3 == 0:
+                time_sleep *= 0.91
+                print(time_sleep)
+            ball.bounce_y()
+            break
 
 screen.exitonclick()
